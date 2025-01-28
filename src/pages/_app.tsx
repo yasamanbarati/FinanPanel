3bff0d0808a642fb7080b2108e0911183740ab76
yasamanbarati <yasamanbarati81@gmail.com>
2025-01-29 00:19:26 +0330

    feat: Improvement code structure(_app)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,23 +1,9 @@
 import type { AppProps } from 'next/app';
-//mui theme provider
-import ThemeProvider from '@/theme';
-//react query provider
+import ThemeProvider from '@/setup/theme';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-// notifactions provider
 import { Toaster } from 'react-hot-toast';
-//redux provider
 
-// css
-// import '@/styles/globals.css';
-// import '@fontsource/roboto/300.css';
-// import '@fontsource/roboto/400.css';
-// import '@fontsource/roboto/500.css';
-// import '@fontsource/roboto/700.css';
-
-// css
-import '@/styles/globals.css';
-// import SettingProvider from '@/context/setting-context';
-//============================================================//
+import '@/setup/styles/globals.css';
 export default function App({ Component, pageProps }: AppProps) {
   const queryClient = new QueryClient({
     defaultOptions: {
@@ -30,13 +16,11 @@ export default function App({ Component, pageProps }: AppProps) {
   }); //
 
   return (
-    // <SettingProvider>
     <QueryClientProvider client={queryClient}>
       <ThemeProvider>
         <Toaster position="bottom-right" reverseOrder={false} />
         <Component {...pageProps} />
       </ThemeProvider>
     </QueryClientProvider>
-    // </SettingProvider>
   );
 }
