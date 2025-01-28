101e0326ab32129c21ad57394154f01052a9885d
yasamanbarati <yasamanbarati81@gmail.com>
2025-01-28 13:17:36 +0330

    feat: development services structure(mock data)

---
+++ src/services/servers/mock.ts
@@ -0,0 +1,87 @@
+export const HEADER_ITEMS = [
+  {
+    title: 'New Income',
+    profit: '$3,850',
+    percent: '8.5%',
+    icon: '/static/icons/card1.svg',
+  },
+  {
+    title: 'Total Income',
+    profit: '$40,689',
+    percent: '16.2%',
+    icon: '/static/icons/card2.svg',
+  },
+  {
+    title: 'New Contracts',
+    profit: '28',
+    percent: '24%',
+    icon: '/static/icons/card3.svg',
+  },
+  {
+    title: 'All Contracts',
+    profit: '28',
+    percent: '24%',
+    icon: '/static/icons/card4.svg',
+  },
+];
+
+export const USERS = [
+  {
+    id: '#345896',
+    name: 'Sofia Miller',
+    email: 'Sofia Miller@gmail.com',
+    count: '12',
+    status: 1,
+    action: 'Deposit',
+    image: '/static/images/user-1.svg',
+    date: 'July 3, 2024',
+    amount: '$45.50',
+  },
+  {
+    id: '#321654',
+    name: 'John Manson',
+    email: 'Ashley Williams@gmail.com',
+    count: '4',
+    status: 0,
+    action: 'Deposit',
+    image: '/static/images/user-2.svg',
+    date: 'July 3, 2024',
+    amount: '$45.50',
+  },
+  {
+    id: '#345896',
+
+    name: 'John Manson',
+    email: 'ohnManson@gmail.com',
+    count: '8',
+    status: 0,
+    action: 'Deposit',
+    image: '/static/images/user-3.svg',
+    date: 'July 3, 2024',
+    amount: '$45.50',
+  },
+  {
+    id: '#345896',
+
+    name: 'John Manson',
+    email: 'Millersoha10@gmail.com',
+    count: '12',
+    status: 1,
+    action: 'Deposit',
+    image: '/static/images/user-4.svg',
+    date: 'July 3, 2024',
+    amount: '$45.50',
+  },
+  {
+    id: '#345896',
+
+    name: 'John Manson',
+    email: 'Millersoha10@gmail.com',
+    count: '12',
+    status: 1,
+    action: 'Deposit',
+    image: '/static/images/user-5.svg',
+    date: 'July 3, 2024',
+    amount: '$45.50',
+  },
+];
