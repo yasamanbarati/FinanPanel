e8bf7013a073079c8162cf7b6036892184fc7558
yasamanbarati <yasamanbarati81@gmail.com>
2025-01-28 13:24:09 +0330

    feat: improvement code structure(IncomeCards)

---
+++ src/components/home/card/IncomeCard.tsx
@@ -0,0 +1,55 @@
+import { palette } from '@/theme/palette';
+import { Box, Typography } from '@mui/material';
+import Image from 'next/image';
+import React from 'react';
+
+interface Props {
+  title: string;
+  profit: string;
+  percent: string;
+  icon: string;
+}
+
+const IncomeCard = ({ title, profit, percent, icon }: Props) => {
+  return (
+    <Box sx={styles.card}>
+      <div style={styles.cardContent}>
+        <div>
+          <Typography variant="body1" component="h3">
+            {title}
+          </Typography>
+          <Typography variant="h4" mt={2}>
+            {profit}
+          </Typography>
+        </div>
+        <Image width={60} height={60} src={icon} alt="icon" />
+      </div>
+      <Typography variant="body1">
+        {percent}
+      </Typography>
+    </Box>
+  );
+};
+
+const styles = {
+  card: {
+    padding: ' 16px 24px',
+    borderRadius: '14px',
+    border: '1px solid rgba(223, 234, 242, 0.5)',
+    background: '#fff',
+    width: '258px',
+    height: '185px',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+  },
+  cardContent: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    '& h3': {
+      color: palette.black.dark,
+    },
+  },
+};
+
+export default IncomeCard;
