311cdffaada40f223e270b17dd76e903116a1dca
yasamanbarati <yasamanbarati81@gmail.com>
2025-02-05 00:23:19 +0330

    feat: development code structure(contractsList Page)

--- src/pages/contracts/list/index.tsx
+++ src/pages/contracts/list/index.tsx
@@ -1,14 +1,57 @@
 import React from 'react';
-import Page from '@/components/Page';
-import MainLayout from '@/layout/mainLayout';
-import ContractList from '@/components/screen/contract/contractList';
+import { Button, Grid, styled } from '@mui/material';
+import MainLayout from '@/layout';
+import BreadcrumbSection from '@/layout/ breadcrumb';
+import ContractListCard from '@/components/pages/contracts/contract-list-card';
+import { ContractList } from '@/services/servers/mock';
+
+const CustomizeDiv = styled('div')(() => ({
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  '& button': {
+    borderRadius: '10px',
+    padding: '16px 24px',
+  },
+}));
 
 export default function ContractListPage() {
   return (
     <MainLayout>
-      <Page title="Contract List">
-        <ContractList />
-      </Page>
+      <CustomizeDiv>
+        <div>
+          <BreadcrumbSection disabledLink="List of contracts" />
+        </div>
+        <Button variant="contained" color="primary">
+          Create New Contract
+        </Button>
+      </CustomizeDiv>
+      <Grid
+        container
+        rowGap={'24px'}
+        columnGap={'16px'}
+        justifyContent="space-between"
+      >
+        {ContractList.map((item, index) => {
+          return (
+            <ContractListCard
+              key={index}
+              contractList={{
+                status: item.status,
+                ImageSrc: item.ImageSrc,
+                title: item.title,
+                time: item.time,
+                minimumAmount: item.minimumAmount,
+                monthlyProfit: item.monthlyProfit,
+                dropDown: item.dropDown,
+                risk: item.risk,
+                limitContrcts: item.limitContrcts,
+                activeContracts: item.activeContracts,
+              }}
+            />
+          );
+        })}
+      </Grid>
     </MainLayout>
   );
 }
