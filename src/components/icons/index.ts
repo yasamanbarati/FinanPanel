2c0fcb823fe494a21b7d3a57b484a9065c6d7703
yasamanbarati <yasamanbarati81@gmail.com>
2025-02-05 00:16:46 +0330

    feat: development code structure(icons)

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,3 +1,7 @@
 export { SearchIcon } from './search_Icon';
 export { SettingIcon } from './setting_icon';
 export { LogOutIcon } from './logout_icon';
+export { NotificationIcon } from './notification-Icon';
+export { ManageAccountIcon } from './manage-account-icon';
+export { PasswordIcon } from './password-icon';
+export { LogOut2Icon } from './logOut2-icon';
---
+++ src/components/icons/logOut2-icon.tsx
@@ -0,0 +1,17 @@
+import { SvgIcon, SvgIconProps } from '@mui/material';
+
+export function LogOut2Icon(props: SvgIconProps) {
+  return (
+    <SvgIcon
+      width="14"
+      height="12"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="m14 6-5 5-1-1V8H4V4h4V1h1l5 4v1Zm-9 5H3c-2 0-3-1-3-3V3c0-1 1-2 3-2h2v1H3L2 3v5l1 1h2v2Z"
+        fill="#667085"
+      />
+    </SvgIcon>
+  );
+}
