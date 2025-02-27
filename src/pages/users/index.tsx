233e1608ad76086444443c43394c9216605e363d
yasamanbarati <yasamanbarati81@gmail.com>
2025-02-27 23:42:21 +0330

    feat: development code structure(usersList Page)

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,11 +1,28 @@
 import React from 'react';
-import Page from '@/components/Page';
 import MainLayout from '@/layout';
+import Head from 'next/head';
+import { Grid, Typography } from '@mui/material';
+import UsersList from '@/components/pages/user-list';
 
 export default function UsersPage() {
   return (
-    <MainLayout>
-      <Page title="Users List">Users</Page>
-    </MainLayout>
+    <>
+      <Head>
+        <title>Fianancial</title>
+      </Head>
+      <MainLayout>
+        <div>
+          <Typography variant="h3" component="h2" mb={2}>
+            Users List
+          </Typography>
+          <Typography variant="body1" component={'p'} mb={12}>
+            Manage your website progress to increase your income.
+          </Typography>
+          <Grid container>
+            <UsersList />
+          </Grid>
+        </div>
+      </MainLayout>
+    </>
   );
 }
